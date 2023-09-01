24696a49faaea602c0ade973411910791115f337
Malongar <malongarfoday@gmail.com>
2023-08-31 23:18:21 -0500

    first commit

---
+++ OneDrive/Desktop/python_data101/python_data_Exercise/13_capitalize/app.py
@@ -0,0 +1,12 @@
+def capitalize(phrase):
+    """Capitalize first letter of first word of phrase.
+
+        >>> capitalize('python')
+        'Python'
+
+        >>> capitalize('only first word')
+        'Only first word'
+    """
+
+    # there's a built-in method for this!
+    return phrase.capitalize()
